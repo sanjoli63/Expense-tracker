f39608d15fcda6da4ef8c7382ee9c84a58a3bfe4
sanjoli63 <sanjoligo6mar@gmail.com>
2020-10-22 12:11:08 +0530

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,47 @@
+import './widgets/user_transaction.dart';
+
+import './widgets/new_transaction.dart';
+import 'package:flutter/material.dart';
+import './widgets/transaction_list.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter App',
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  // String titleInput;
+  // String amountInput;
+  final titleController = TextEditingController();
+  final amountController = TextEditingController();
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Flutter App'),
+      ),
+      body: Column(
+        // mainAxisAlignment: MainAxisAlignment.start,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: <Widget>[
+          Container(
+            width: double.infinity,
+            child: Card(
+              child: Text('Chart!'),
+              color: Colors.pink,
+              elevation: 6,
+            ),
+          ),
+          UserTransaction()
+        ],
+      ),
+    );
+  }
+}
