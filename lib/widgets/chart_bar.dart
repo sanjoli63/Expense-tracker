9fb67871d184ba87c37b49c60e9ece498b1babc7
sanjoli63 <sanjoligo6mar@gmail.com>
2021-03-10 14:03:06 +0530

    media

--- lib/widgets/chart_bar.dart
+++ lib/widgets/chart_bar.dart
@@ -9,46 +9,52 @@ class ChartBar extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(children: <Widget>[
-      Container(
-        height: 20,
-        child: FittedBox(
-          child: Text('\$${spendingAmount.toStringAsFixed(2)}'),
-        ),
-      ),
-      SizedBox(
-        height: 5,
-      ),
-      Container(
-          height: 60,
-          width: 10,
-          child: Stack(
-            children: <Widget>[
-              Container(
-                decoration: BoxDecoration(
-                  border: Border.all(
-                    color: Colors.grey,
-                    width: 1.0,
-                  ),
-                  color: Color.fromRGBO(220, 220, 220, 1),
-                  borderRadius: BorderRadius.circular(10),
-                ),
+    return LayoutBuilder(
+      builder: (ctx, constraint) {
+        return Column(
+          children: <Widget>[
+            Container(
+              height: 20,
+              child: FittedBox(
+                child: Text('\$${spendingAmount.toStringAsFixed(2)}'),
               ),
-              FractionallySizedBox(
-                heightFactor: spendingPctOfTotal,
-                child: Container(
-                  decoration: BoxDecoration(
-                    color: Colors.indigo,
-                    borderRadius: BorderRadius.circular(10),
-                  ),
-                ),
-              ),
-            ],
-          )),
-      SizedBox(
-        height: 5,
-      ),
-      Text(label),
-    ]);
+            ),
+            SizedBox(
+              height: 5,
+            ),
+            Container(
+                height: constraint.maxHeight * 0.7,
+                width: 10,
+                child: Stack(
+                  children: <Widget>[
+                    Container(
+                      decoration: BoxDecoration(
+                        border: Border.all(
+                          color: Colors.grey,
+                          width: 1.0,
+                        ),
+                        color: Color.fromRGBO(220, 220, 220, 1),
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                    ),
+                    FractionallySizedBox(
+                      heightFactor: spendingPctOfTotal,
+                      child: Container(
+                        decoration: BoxDecoration(
+                          color: Colors.indigo,
+                          borderRadius: BorderRadius.circular(10),
+                        ),
+                      ),
+                    ),
+                  ],
+                )),
+            SizedBox(
+              height: 5,
+            ),
+            Text(label),
+          ],
+        );
+      },
+    );
   }
 }
