f9cab498a2cdbe2a4211d4c8b136e2563e17de24
sanjoli63 <sanjoligo6mar@gmail.com>
2021-05-10 12:45:07 +0530

    show chart

--- lib/main.dart
+++ lib/main.dart
@@ -161,7 +161,8 @@ class _MyHomePageState extends State<MyHomePage> {
                 Row(
                   mainAxisAlignment: MainAxisAlignment.center,
                   children: <Widget>[
-                    Text('Show Chart'),
+                    Text('Show Chart',
+                        style: Theme.of(context).textTheme.headline6),
                     Switch.adaptive(
                       activeColor: Theme.of(context).accentColor,
                       value: _showChart,
